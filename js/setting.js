0199c566c157028ac0e6c6459a88d687f52a4731
smagch <smagch@gmail.com>
2013-07-18 00:44:52 +0900

    change debt icon

--- js/setting.js
+++ js/setting.js
@@ -77,8 +77,8 @@ OpenSpending.Styles.Cofog = {
   '11': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#790586' },
   '135': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#790586' },
   // 12 公債費
-  '12': { icon: 'icons/public-debt.svg', color: '#C75746', bcolor: '#790586' },
-  '136': { icon: 'icons/public-debt.svg', color: '#C75746', bcolor: '#790586' },
+  '12': { icon: 'icons/debt-asahikawa.svg', color: '#C75746', bcolor: '#790586' },
+  '136': { icon: 'icons/debt-asahikawa.svg', color: '#C75746', bcolor: '#790586' },
   // 13 職員費
   '13': { icon: 'icons/payroll.svg', color: '#C75746', bcolor: '#790586' },
   '137': { icon: 'icons/payroll.svg', color: '#C75746', bcolor: '#790586' },
